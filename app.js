0fd85fad3d262a669c4726403c65d632af388b65
JLambCode <66347648+SaveJL101@users.noreply.github.com>
2021-06-11 21:54:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+
   
