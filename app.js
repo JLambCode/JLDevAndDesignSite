80a781fe7f392d4bc2274db5d652ce376452e888
Jeremy <66347648+JLambCode@users.noreply.github.com>
2021-12-21 22:08:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home");
+
+const faders = document.querySelectorAll('.fade-in');
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(
+    entries,
+    sectionOneObserver
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.classList.add("nav-scrolled");
+        } else {
+            header.classList.remove("nav-scrolled")
+        }
+    });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {};
+
+const appearOnScroll = new IntersectionObserver(
+    function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, appearOptions
+);
 
   
